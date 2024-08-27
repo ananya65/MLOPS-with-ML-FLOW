e1a2ea6c55641dcb4876d22c20cb8114f807ea7a
Pandey, Ananya <Ananya.Pandey@uhs.nhs.uk>
2024-08-27 17:31:41 +0100

    added the validation and transformer stage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from MLOPSproject import logger
 from MLOPSproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLOPSproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLOPSproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME ="Data Ingestion Stage"
@@ -13,3 +15,31 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+    
+
+STAGE_NAME ="Data Validation Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- src/MLOPSproject/config/configuration.py
+++ src/MLOPSproject/config/configuration.py
@@ -1,6 +1,6 @@
 from MLOPSproject.constants import *
 from MLOPSproject.utils.common import read_yaml,create_directories
-from MLOPSproject.entity.config_entity import DataIngestionConfig
+from MLOPSproject.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 
 
 class ConfigurationManager:
@@ -25,3 +25,26 @@ class ConfigurationManager:
         data_ingestion_config= DataIngestionConfig(root_dir=config.root_dir,source_URL=config.source_URL,local_data_file=config.local_data_file,unzip_dir=config.unzip_dir)
         
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config =self.config.data_validation
+        schema=self.schema.COLUMNS
+        create_directories([config.root_dir])
+        
+        data_validation_config= DataValidationConfig(root_dir=config.root_dir,STATUS_FILE=config.STATUS_FILE,unzip_data_dir=config.unzip_data_dir,all_schema=schema)
+        
+        return data_validation_config
+    
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
